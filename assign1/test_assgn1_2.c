486681717c367545979ed05c849baf5f23d8a81e
tejasGangadharaiah <93666099+tejasGangadharaiah@users.noreply.github.com>
2023-02-10 01:41:54 -0600

    Update test_assgn1_2.c
    bug fixed

--- assign1/test_assgn1_2.c
+++ assign1/test_assgn1_2.c
@@ -24,27 +24,59 @@ int main()
     char filename[100]= "/Users/daniel/c/525/assign1/page2.txt";
 
 
+#ifndef __DEBUG__
+    printf("\n>>>> Invoking createPageFile [PAGE_SIZE: %d, mgmtInfo: %lu]", PAGE_SIZE, sizeof(MGMT_Info));
+#endif    
     rc = createPageFile(filename);
 
+#ifndef __DEBUG__
+    printf("\n>>>> Invoking openPageFile");
+#endif    
     openPageFile(filename, &fHandle);
     
     memPage = (SM_PageHandle)malloc(PAGE_SIZE);
-    rc = readBlock(1, &fHandle, memPage);
+#ifndef __DEBUG__
+    printf("\n[fHandle]totalNumPages: %d, getBlockPos: %d" , fHandle.totalNumPages, getBlockPos(&fHandle)); printf("\n");
+    printf("\n>>>> Invoking readBlock");
+#endif    
+    rc = readBlock(0, &fHandle, memPage);
+#ifndef __DEBUG__
+    printf("\n[fHandle]totalNumPages: %d, getBlockPos: %d" , fHandle.totalNumPages, getBlockPos(&fHandle)); printf("\n");
+    printf("\n>>>> Invoking readFirstBlock");
+#endif    
     rc = readFirstBlock(&fHandle, memPage);
+#ifndef __DEBUG__
+    printf("\n[fHandle]totalNumPages: %d, getBlockPos: %d" , fHandle.totalNumPages, getBlockPos(&fHandle)); printf("\n");
+    printf("\n>>>> Invoking writeBlock");
+#endif    
     rc = writeBlock(0, &fHandle, memPage);
+#ifndef __DEBUG__
+    printf("\n[fHandle]totalNumPages: %d, getBlockPos: %d" , fHandle.totalNumPages, getBlockPos(&fHandle)); printf("\n");
+    printf("\n>>>> Invoking readPreviousBlock");
+#endif    
     rc = readPreviousBlock(&fHandle, memPage);
+#ifndef __DEBUG__
+    printf("\n[fHandle]totalNumPages: %d, getBlockPos: %d" , fHandle.totalNumPages, getBlockPos(&fHandle)); printf("\n");
+    printf("\n>>>> Invoking readCurrentBlock");
+#endif    
     rc = readCurrentBlock(&fHandle, memPage);
+#ifndef __DEBUG__
+    printf("\n[fHandle]totalNumPages: %d, getBlockPos: %d" , fHandle.totalNumPages, getBlockPos(&fHandle)); printf("\n");
+    printf("\n>>>> Invoking readNextBlock");
+#endif    
     rc = readNextBlock(&fHandle, memPage);
+#ifndef __DEBUG__
+    printf("\n[fHandle]totalNumPages: %d, getBlockPos: %d" , fHandle.totalNumPages, getBlockPos(&fHandle)); printf("\n");
+    printf("\n>>>> Invoking readLastBlock");
+#endif    
     rc = readLastBlock(&fHandle, memPage);
 
-    #ifdef __DEBUG__
-    printf("\n[fHandle] fileName: %s, totalNumPages: %d, getBlockPos: %d"
-            , fHandle.fileName
-            , fHandle.totalNumPages
-            , getBlockPos(&fHandle));
-    printf("\n"); 
-    #endif
+#ifndef __DEBUG__
+    printf("\n[fHandle]totalNumPages: %d, getBlockPos: %d" , fHandle.totalNumPages, getBlockPos(&fHandle)); printf("\n");
+#endif
 
+    #ifndef __DEBUG__
     fclose(fHandle.mgmtInfo.fp);
     return rc;
+    #endif
 }
